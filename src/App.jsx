1b37231fde97a19ae6ccd710a2e70e5dbd3e92f1
Mary <gretchen7887@gmail.com>
2024-02-02 21:48:35 +0400

    Upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import {Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Header from './Components/Header'
 import Home from './Pages/Home'
+import Cart from './Pages/Cart'
 import NotFound from './Pages/NotFound'
 
 import './scss/app.scss'
@@ -14,9 +15,9 @@ function App() {
       <div className="content">
         <div className="container">
           <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/" element={<Home/>} />
-            <Route path="/not-found" element={<NotFound/>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart/>} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,5 @@
 // import React from 'react'
+import {Link} from 'react-router-dom'
 
 import logo from '../assets/pizza-logo.svg'
 
@@ -6,15 +7,17 @@ const Header = () => {
   return (
     <div className="header">
       <div className="container">
-        <div className="header__logo">
-          <img width="38" src={logo} alt="Pizza logo" />
-          <div>
-            <h1>React Pizza</h1>
-            <p>The taste you will not forget</p>
+        <Link to='/'>
+          <div className="header__logo">
+            <img width="38" src={logo} alt="Pizza logo" />
+            <div>
+              <h1>React Pizza</h1>
+              <p>The taste you will not forget</p>
+            </div>
           </div>
-        </div>
+        </Link>
         <div className="header__cart">
-          <a href="/cart.html" className="button button--cart">
+          <Link to='/cart' className="button button--cart">
             <span>520 ₽</span>
             <div className="button__delimiter"></div>
             <svg
@@ -47,7 +50,7 @@ const Header = () => {
               />
             </svg>
             <span>3</span>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
