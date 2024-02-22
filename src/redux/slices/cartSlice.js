ddc75f915d89bae51d9846696da5557fa5a815ff
Mary <gretchen7887@gmail.com>
2024-02-22 20:03:40 +0400

    Upd

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -15,7 +15,7 @@ const cartSlice = createSlice({
     //     return obj.price + sum
     //   }, 0)
     // },
-    
+
     addItem(state, action) {
       const findItem = state.items.find((obj) => obj.id === action.payload.id)
 
@@ -28,7 +28,7 @@ const cartSlice = createSlice({
         })
       }
       state.totalPrice = state.items.reduce((sum, obj) => {
-        return obj.price + sum
+        return obj.price * obj.count + sum
       }, 0)
     },
     removeItem(state, action) {
