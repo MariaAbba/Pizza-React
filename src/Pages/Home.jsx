524d5164a2d7af78ff4ed4398a880471d35815d9
Mary <gretchen7887@gmail.com>
2024-03-03 22:12:55 +0400

    Added try /catch

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -44,13 +44,18 @@ const Home = () => {
     const order = sortType.includes('-') ? 'asc' : 'desc'
     const search = searchValue ? `&search=${searchValue}` : ''
 
-    const res = await axios.get(
-      `https://65bcb01fb51f9b29e9320d4c.mockapi.io/items?&page=${currentPage}&limit=6&${
-        categoryId > 0 ? `category=${categoryId}` : ''
-      }&sortBy=${sortType.replace('-', '')}&order=${order}${search}`
-    )
-    setItems(res.data)
-    setIsLoading(false)
+    try {
+      const res = await axios.get(
+        `https://65bcb01fb51f9b29e9320d4c.mockapi.io/items?&page=${currentPage}&limit=6&${
+          categoryId > 0 ? `category=${categoryId}` : ''
+        }&sortBy=${sortType.replace('-', '')}&order=${order}${search}`
+      )
+      setItems(res.data)
+      setIsLoading(false)
+    } catch (error) {
+      alert('Something went wrong')
+    }
+    window.scrollTo(0, 0)
   }
 
   React.useEffect(() => {
@@ -71,7 +76,6 @@ const Home = () => {
 
   React.useEffect(() => {
     window.scrollTo(0, 0)
-
     if (!isSearch.current) {
       fetchPizzas()
     }
